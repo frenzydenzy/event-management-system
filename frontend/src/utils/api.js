663aab06597decef6d8286d78f2ed0481c041c86
frenzydenzy <hardikbansal360@gmail.com>
2026-03-04 19:38:39 +0530

    Updated login functionality

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,15 +1,20 @@
 import axios from "axios";
 
-// centralised axios instance, sets baseURL and attaches token automatically
+// Vite environment variable
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+
+// centralised axios instance
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000",
+  baseURL: API_URL,
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
+
   if (token) {
     config.headers.Authorization = token;
   }
+
   return config;
 });
 
